cf70375f15675c6cf141311c4f28f4e001e47154
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-06-10 19:34:56 +0100

    FEAT: Initial dependencies and basic files set up

---
+++ mongo.js
@@ -0,0 +1,14 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb+srv://firstuser:Q60lMT1HElo@letslearn.ckbhttu.mongodb.net/products_test?retryWrites=true&w=majority&appName=LetsLearn';
+
+const createProduct = async (req, res, next) => {
+
+};
+
+const getProducts = async (req, res, next) => {
+
+};
+
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
