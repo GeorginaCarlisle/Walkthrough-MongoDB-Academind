076f6f9b344a60298b9e5745e9ea667265ce15d5
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-06-10 20:57:01 +0100

    FEAT: Functionality to create new products added

--- mongo.js
+++ mongo.js
@@ -1,9 +1,25 @@
 const MongoClient = require('mongodb').MongoClient;
 
-const url = 'mongodb+srv://firstuser:Q60lMT1HElo@letslearn.ckbhttu.mongodb.net/products_test?retryWrites=true&w=majority&appName=LetsLearn';
+const url = 'mongodb+srv://firstuser:Q60lMT1HElo@letslearn.ckbhttu.mongodb.net/?retryWrites=true&w=majority&appName=LetsLearn';
 
 const createProduct = async (req, res, next) => {
+  const newProduct = {
+    name: req.body.name,
+    proce: req.body.price
+  };
+  const client = new MongoClient(url);
 
+  try {
+    await client.connect();
+    const db = client.db();
+    const result = db.collection('products').insertOne(newProduct);
+  } catch (error) {
+    return res.json({message: 'Could not store data.'});
+  };
+  setTimeout(()=>{
+    client.close();
+  }, 1500);
+  res.json(newProduct);
 };
 
 const getProducts = async (req, res, next) => {
